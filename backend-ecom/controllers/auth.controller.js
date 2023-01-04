d61e4a9ade73fc57776e69b15313810d9b2433ed
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2023-01-04 22:06:06 +0530

    login, logout routes

--- backend-ecom/controllers/auth.controller.js
+++ backend-ecom/controllers/auth.controller.js
@@ -41,3 +41,62 @@ export const signUp = asyncHandler(async (req, res,) => {
     })
 
 })
+
+
+/********* 
+* @SIGNIN
+* @route http://localhost:6000/api/auth/signin
+* @discription User signin controller for logging user
+@parameters name,email, password
+@returns User Object
+
+**************/
+
+export const login = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email || !password) {
+        throw new CustomError("Please fill all fields")
+    }
+    const user = User.findOne({email}).select("+password")
+
+    if(!user) {
+        throw new CustomError('Invalid Credentials', 400)
+    }
+    const passwordMatch = await user.comparePassword(password)
+
+    if(passwordMatch){
+        const token = user.getJwtToken()
+        user.password = undefined
+        res.cookie("token",token, cookieOptions)
+        res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+})
+
+/********* 
+* @LOGOUT
+* @route http://localhost:6000/api/auth/login
+* @discription User logout  controller for logging out user by clearing user cookies
+@parameters 
+@returns  success message
+
+**************/
+
+export const logout = asyncHandler(async(req, res) => {
+    // res.clearCookie()
+    res.cookie('token',null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+
+    })
+    res.status(200).json({
+        success: true,
+        message: "Logget Out"
+
+    })
+})
